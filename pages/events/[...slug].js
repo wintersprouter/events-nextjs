269c05572d64992ec83b4a3965dd55f65713433b
wintersprouter <bornonefreekiss@alum.ccu.edu.tw>
2022-04-17 22:28:38 +0800

    feat: extracting data on the catch-all page

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,4 +1,34 @@
+import { useRouter } from 'next/router'
+import { getFilteredEvents } from '../../dummy-data'
 function FilteredEventsPage() {
+  const router = useRouter()
+  const filterData = router.query.slug
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>
+  }
+
+  const filteredYear = filterData[0]
+  const filteredMonth = filterData[1]
+
+  const numYear = +filteredYear
+  const numMonth = +filteredMonth
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear < 2021 ||
+    numYear > 2022 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p> Invalid filter. Please adjust your values!</p>
+  }
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth })
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events found for the chosen filter!</p>
+  }
+
   return (
     <div>
       <h1>Filtered Events Page</h1>
