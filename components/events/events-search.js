9ec642ae15a56ac9327a39dfa8230f1a844b0cf6
wintersprouter <bornonefreekiss@alum.ccu.edu.tw>
2022-04-17 21:59:44 +0800

    feat: adding a filter form for filtering events

---
+++ components/events/events-search.js
@@ -0,0 +1,37 @@
+import Button from '../ui/button'
+import classes from './events-search.module.css'
+
+function EventsSearch(props) {
+  return (
+    <form className={classes.form}>
+      <div className={classes.controls}>
+        <div className={classes.control}>
+          <label htmlFor="year">Year</label>
+          <select id="year">
+            <option value="2021">2021</option>
+            <option value="2022">2022</option>
+          </select>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="month">Month</label>
+          <select id="year">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+            <option value="6">6</option>
+            <option value="7">7</option>
+            <option value="8">8</option>
+            <option value="9">9</option>
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+        </div>
+      </div>
+      <Button>Find Event</Button>
+    </form>
+  )
+}
+export default EventsSearch
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,11 +1,14 @@
 import { getAllEvents } from '../../dummy-data'
 import EventList from '../../components/events/event-list'
+import EventsSearch from '../../components/events/events-search'
+import { Fragment } from 'react/cjs/react.production.min'
 function AllEventsPage() {
   const events = getAllEvents()
   return (
-    <div>
+    <Fragment>
+      <EventsSearch />
       <EventList items={events} />
-    </div>
+    </Fragment>
   )
 }
 
